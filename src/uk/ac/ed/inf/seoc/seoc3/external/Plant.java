7ff12cd748fe75a1cdb2ed4e8cd6b78fff061019
Guy Taylor <thebigguy.co.uk@gmail.com>
2010-11-20 19:58:34 +0000

    Added some docs to the interface

--- src/uk/ac/ed/inf/seoc/seoc3/external/Plant.java
+++ src/uk/ac/ed/inf/seoc/seoc3/external/Plant.java
@@ -11,8 +11,20 @@ import java.util.Date;
  */
 public interface Plant {
 	
+	/**
+	 * @param product		The ID of the product to produce
+	 * @param quantity		The quantity of the product to produce
+	 * @return				The ID to an OrderStatus
+	 * @throws RequestError
+	 */
 	public int requestProduction(Product product, int quantity) throws RequestError;
 	
+	/**
+	 * @param product		The ID of the product to produce
+	 * @param quantity		The quantity of the product to produce
+	 * @return				The estimated Date and Time that this could be produced on.
+	 * @throws RequestError
+	 */
 	public Date requestProductionEstimate(Product product, int quantity) throws RequestError;
 	
 	public OrderStatus getStatus(int uid) throws RequestError;
