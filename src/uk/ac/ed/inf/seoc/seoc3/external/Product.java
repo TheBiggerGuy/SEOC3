da4cf9070be408bdedde4e19f725b53955cd2841
Guy Taylor <thebigguy.co.uk@gmail.com>
2010-11-22 11:35:42 +0000

    Changed isChilled, isFrozen ... to string[] getProperties() at request of transportation group

--- src/uk/ac/ed/inf/seoc/seoc3/external/Product.java
+++ src/uk/ac/ed/inf/seoc/seoc3/external/Product.java
@@ -9,15 +9,24 @@ package uk.ac.ed.inf.seoc.seoc3.external;
  */
 public interface Product {
 	
+	/** A unique int that will constatntly represent this product */
 	public int getProductID();
 	
+	/** The name of the product (This may not be unique) */
 	public String getName();
 	
+	/** The quantity of products that an order must be divisable by and also above */
 	public int getOrderQuantityModulo();
-
-	public boolean isFragile();
-	public boolean isChilled();
-	public boolean isFrozen();
-	public boolean isFlammable();
-
+	
+	/**
+	    A list of aditinal properties<br />
+	    such as:<br/>
+	    <ul>
+	        <li>Fragile</li>
+	        <li>Chilled</li>
+	        <li>Frozen</li>
+	        <li>Flammable</li>
+	    </ul>
+	*/
+    public ArrayList<String> getProperties()
 }
