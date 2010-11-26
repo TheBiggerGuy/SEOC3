d6513ba2fc325b23d817cb6d23aa288762343ecd
Guy Taylor <thebigguy.co.uk@gmail.com>
2010-11-26 01:06:30 +0000

    Semi-working factory!!!!

--- src/uk/ac/ed/inf/seoc/seoc3/plant/internal/PlantFactory.java
+++ src/uk/ac/ed/inf/seoc/seoc3/plant/internal/PlantFactory.java
@@ -1,14 +1,61 @@
 package uk.ac.ed.inf.seoc.seoc3.plant.internal;
 
-public class PlantFactory {
+import java.util.LinkedList;
+import java.util.Queue;
+
+import org.apache.log4j.Logger;
+
+public class PlantFactory implements Runnable {
+	
+	PlantLocation location;
+	String name;
+	int id;
+	Logger log;
+	
+	Thread thread;
+	
+	Queue<Production> productionQueue = new LinkedList<Production>();
 	
 	protected PlantFactory(int ID, String name, PlantLocation location){
+		this.id = ID;
+		this.location = location;
+		this.name = name;
 		
+		this.log = Logger.getLogger(PlantFactory.class);
+		
+		thread = new Thread(this);
+		thread.start();
 	}
 	
 	protected boolean doesProduce(PlantProduct product){
 		// TODO:
 		return false;
+	}
+
+	@Override
+	public void run() {
+		Thread thisThread = Thread.currentThread();
+
+		while(thisThread == thread){
+			
+			log.debug("Factory "+id+" is checking queue");
+			
+			Production head = productionQueue.peek();
+			
+			if(head != null && head.isFinished())
+				productionQueue.poll();
+			
+			try {
+				Thread.sleep( 60 * 1000 );
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	protected void stop(){
+		thread = null;
 	}
 
 }
---
+++ src/uk/ac/ed/inf/seoc/seoc3/plant/internal/Production.java
@@ -0,0 +1,9 @@
+package uk.ac.ed.inf.seoc.seoc3.plant.internal;
+
+public class Production {
+	
+	protected Boolean isFinished(){
+		return false;
+	}
+
+}
